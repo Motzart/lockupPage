21d12fa91268d4d76139d8786d803c2ab77cf124
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-04-28 14:42:57 +0300

    add total

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { separation, toPrecision, toReadableNumber } from '~utils/numbers';
 import { useSortableData } from '~hooks/useSortableData';
+import styles from './content.module.scss';
 
 type Props = {
-  lookups: []
-}
+  lookups: [];
+};
 
 export const Content = (props: Props) => {
   const { lookups } = props;
@@ -18,61 +19,130 @@ export const Content = (props: Props) => {
     return sortConfig.key === name ? sortConfig.direction : undefined;
   };
 
+  const balance = (data: any, field: any): any => {
+    let total: number = 0;
+    data.map((item: any) => {
+      const number = toPrecision(toReadableNumber(18, item[1][field]), 0);
+      total += Number(number);
+    });
+
+    return total;
+  };
+
   return (
     <div className="container pt-1">
       <div className="row">
         <div className="col-md-12">
-          <br/>
           <br />
-          {showData(items, requestSort, getClassNamesFor)}
+          <br />
+          {showData(items, requestSort, getClassNamesFor, balance, sortConfig)}
         </div>
       </div>
     </div>
   );
-}
+};
 
-const showData = (data: any, requestSort: Function, getClassNamesFor: Function) => {
+const showData = (
+  data: any,
+  requestSort: Function,
+  getClassNamesFor: Function,
+  balance: any,
+  sortConfig: any
+) => {
   return (
     <div>
       <table className="table">
         <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Account Id</th>
-          <th scope="col"
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col" style={{ backgroundColor: '#eaeaea' }}>
+              Account Id
+            </th>
+            <th
+              scope="col"
               onClick={() => requestSort('claimed_balance')}
-              className={getClassNamesFor('claimed_balance')}
-          >
-            Claimed Balance
-          </th>
-          <th scope="col"
+              className={
+                getClassNamesFor('claimed_balance') === undefined
+                  ? styles.scending
+                  : getClassNamesFor('claimed_balance') === 'ascending'
+                  ? styles.ascending
+                  : styles.descending
+              }
+              style={{ cursor: 'pointer', backgroundColor: 'lightgray' }}
+            >
+              Claimed Balance
+            </th>
+            <th
+              scope="col"
               onClick={() => requestSort('total_balance')}
-              className={getClassNamesFor('total_balance')}
-          >
-            Total Balance
-          </th>
-          <th scope="col"
+              className={
+                getClassNamesFor('total_balance') === undefined
+                  ? styles.scending
+                  : getClassNamesFor('total_balance') === 'ascending'
+                  ? styles.ascending
+                  : styles.descending
+              }
+              style={{ cursor: 'pointer', backgroundColor: 'darkgrey' }}
+            >
+              Total Balance
+            </th>
+            <th
+              scope="col"
               onClick={() => requestSort('unclaimed_balance')}
-              className={getClassNamesFor('unclaimed_balance')}
-          >
-            Unclaimed Balance
-          </th>
-        </tr>
+              className={
+                getClassNamesFor('unclaimed_balance') === undefined
+                  ? styles.scending
+                  : getClassNamesFor('unclaimed_balance') === 'ascending'
+                  ? styles.ascending
+                  : styles.descending
+              }
+              style={{ cursor: 'pointer', backgroundColor: 'dimgray' }}
+            >
+              Unclaimed Balance
+            </th>
+          </tr>
         </thead>
         <tbody>
-        {data.map((item, index) => {
-          return (
-            <tr key={item[1].account_id + index}>
-              <th scope="row">{index + 1}</th>
-              <td>{item[1].account_id}</td>
-              <td>{separation(toPrecision(toReadableNumber(18, item[1].claimed_balance), 0))}</td>
-              <td>{separation(toPrecision(toReadableNumber(18, item[1].total_balance), 0))}</td>
-              <td>{separation(toPrecision(toReadableNumber(18, item[1].unclaimed_balance), 0))}</td>
-            </tr>
-          )
-        })}
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Total</th>
+            <th scope="col">{separation(balance(data, 'claimed_balance'))}</th>
+            <th scope="col">{separation(balance(data, 'total_balance'))}</th>
+            <th scope="col">
+              {separation(balance(data, 'unclaimed_balance'))}
+            </th>
+          </tr>
+          {data.map((item, index) => {
+            return (
+              <tr key={item[1].account_id + index}>
+                <th scope="row">{index + 1}</th>
+                <td>{item[1].account_id}</td>
+                <td>
+                  {separation(
+                    toPrecision(
+                      toReadableNumber(18, item[1].claimed_balance),
+                      0
+                    )
+                  )}
+                </td>
+                <td>
+                  {separation(
+                    toPrecision(toReadableNumber(18, item[1].total_balance), 0)
+                  )}
+                </td>
+                <td>
+                  {separation(
+                    toPrecision(
+                      toReadableNumber(18, item[1].unclaimed_balance),
+                      0
+                    )
+                  )}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
